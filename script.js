67310805aea647d26ebc90a35c7069d998c3da1e
ebs <ebs.m93@gmail.com>
2022-07-13 06:51:35 -0700

    touch to expand

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// document.getElementById('vid').play();
+const images = document.querySelectorAll('img');
+images.forEach(i => {
+	i.addEventListener('click',clickImage);
+}); 
+
+function clickImage() {
+	// console.log('click');
+	// console.log(this.src);
+    this.classList.toggle('clicked-image')
+}
